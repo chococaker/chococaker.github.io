fc9cf81d0fd65e645832f25e7f0cdf6aa088404d
chococaker <152650318+chococaker@users.noreply.github.com>
2026-06-27 11:33:47 -0500

    Initial commit

---
+++ script/nav.js
@@ -0,0 +1,42 @@
+function navigateTo(location) {
+    document.location = "/" + location;
+}
+
+function keyupLinking(e) {
+    e = e || window.event;
+
+    switch (e.key) {
+        case "h":
+            navigateTo("index");
+            break;
+
+        case "a":
+            navigateTo("about");
+            break;
+
+        case "b":
+            navigateTo("blog");
+            break;
+
+        case "d":
+            navigateTo("docs");
+            break;
+    }
+}
+
+function keydownLinking(e) {
+    e = e || window.event;
+
+    switch (e.key) {
+        case "h":
+            document.getElementById("home-link").classList.add("key-selected");
+            break;
+
+        case "a":
+            document.getElementById("about-link").classList.add("key-selected");
+            break;
+    }
+}
+
+document.onkeydown = keydownLinking;
+document.onkeyup = keyupLinking;
